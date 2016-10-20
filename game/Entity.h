dde51b9e79c3198871898a999d07a1c76e527dc9
Harry Wass <harboym@gmail.com>
2016-10-20 13:19:04 -0400

    experiencePoints variable added to Entity.h

--- game/Entity.h
+++ game/Entity.h
@@ -135,6 +135,7 @@ public:
 	idList< idEntityPtr<idEntity> >	targets;		// when this entity is activated these entities entity are activated
 
 	int						health;					// FIXME: do all objects really need health?
+	int						experiencePoints;		/// Experience points declared here for lack of a better place
 
 // RAVEN BEGIN
 // ddynerman: optional pre-prediction
